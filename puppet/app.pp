25979f1ae2a68d0b98270588e959842517b3319f
Jason Edgecombe <jason@rampaginggeek.com>
2014-12-14 20:29:20 -0500

    Create a symlink to work around the missing /bin/test
    Fixes the following error:
        ==> demo: Error: /Stage[main]/Dnsmasq/Exec[reload_resolvconf]: Could not evaluate: /bin/sh: 1: /bin/test: not found

--- puppet/app.pp
+++ puppet/app.pp
@@ -1,5 +1,10 @@
 Exec { path => "/usr/bin:/usr/sbin:/bin:/sbin" }
 
+# fix dnsmasq, which looks for /bin/test
+file { '/bin/test':
+   ensure => 'link',
+   target => '/usr/bin/test',
+}
 
 stage { 'preinstall':
   before => Stage['main']
